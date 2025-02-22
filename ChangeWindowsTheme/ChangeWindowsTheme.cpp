21c98bd6f1d10bf478789b8314ac5140b420ba68
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-02-22 22:01:51 +0330

    Update ChangeWindowsTheme.cpp

--- ChangeWindowsTheme/ChangeWindowsTheme.cpp
+++ ChangeWindowsTheme/ChangeWindowsTheme.cpp
@@ -6,10 +6,6 @@
 #include <string>
 #include <winreg.h>
 
-//const int HWND_BROADCAST = 0xFFFF;
-//const int WM_SETTINGCHANGE = 0x1A;
-//const int SMTO_ABORTIFHUNG = 0x0002;
-
 // Path to the registry key
 const std::wstring keyPath = L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
 const std::wstring valueNameApps = L"AppsUseLightTheme";
@@ -62,14 +58,3 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
